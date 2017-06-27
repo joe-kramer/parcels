0c52a7bae9cd8cc351c0eeba4eba443cee504a88
Joe Kramer <joekramer1125@gmail.com>
2017-06-27 16:38:15 -0700

    set up user inputs and construct Parcel object

--- App.java
+++ App.java
@@ -1,5 +1,22 @@
+import java.io.Console;
+
 class App {
   public static void main(String[] args) {
-    
+    Console myConsole = System.console();
+    boolean running = false;
+
+    while(running) {
+      System.out.println("Please enter your dimensions. First, Width:");
+      int userWidth = Integer.parseInt(myConsole.readLine());
+      System.out.println("Height:");
+      int userHeight = Integer.parseInt(myConsole.readLine());
+      System.out.println("Length:");
+      int userLength = Integer.parseInt(myConsole.readLine());
+      System.out.println("Weight:");
+      int userWeight = Integer.parseInt(myConsole.readLine());
+
+
+      Parcel userParcel = new Parcel(userWidth, userHeight, userLength, userWeight);
+    }
   }
 }
